1e658095cc96481a3a1b994d5b7912ee85fea806
dipanshurai.11@gmail.com <dipanshurai.11@gmail.com>
2022-12-18 12:39:27 +0530

    added cred

--- state.tf
+++ state.tf
@@ -9,5 +9,6 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-    profile = "default"
+    access_key = "AKIAQRVBO4Z6Q6VLL5F6"
+    secret_key = "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"
 }
